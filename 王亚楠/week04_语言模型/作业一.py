f9f5b7b17456e126ab1684ab6e8b35c4b2ff2f67
wynstrive <87252773+wynstrive@users.noreply.github.com>
2026-05-14 10:06:59 +0800

    Update 作业一.py

--- 王亚楠/week04_语言模型/作业一.py
+++ 王亚楠/week04_语言模型/作业一.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class TransformerBlock(nn.Module):
   def __init__(self, embed_dim, num_heads, ff_dim, dropout):
     super().__init__()
-    self.mha = nn.MultiheadAttention(embed_dim, num_heas, nn.Dropout(dropout))
+    self.mha = nn.MultiheadAttention(embed_dim, num_heas, dropout)
     self.norm1 = nn.LayerNorm(embed_dim)
     self.norm2 = nn.LayerNorm(embed_dim)
     self.ffn = nn.sequential(nn.Linear(embed_dim, ff_dim), nn.Dropout(droport), nn.Linear(ff_dim, embed_dim), nn.Dropout(dropout))
