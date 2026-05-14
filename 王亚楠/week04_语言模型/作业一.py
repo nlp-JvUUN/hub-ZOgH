ae47db201d8a9467724192a5d85b9951b3f590f8
wynstrive <87252773+wynstrive@users.noreply.github.com>
2026-05-14 10:04:54 +0800

    Implement TransformerBlock with attention mechanism
    Fix typos in TransformerBlock class and its methods.

--- 王亚楠/week04_语言模型/作业一.py
+++ 王亚楠/week04_语言模型/作业一.py
@@ -1,1 +1,15 @@
+import torch
+import torch.nn as nn
 
+class TransformerBlock(nn.Module):
+  def __init__(self, embed_dim, num_heads, ff_dim, dropout):
+    super().__init__()
+    self.mha = nn.MultiheadAttention(embed_dim, num_heas, nn.Dropout(dropout))
+    self.norm1 = nn.LayerNorm(embed_dim)
+    self.norm2 = nn.LayerNorm(embed_dim)
+    self.ffn = nn.sequential(nn.Linear(embed_dim, ff_dim), nn.Dropout(droport), nn.Linear(ff_dim, embed_dim), nn.Dropout(dropout))
+  def forward(self, x, mask = None):
+    mha_out = self.mah(self.norm1(x), mask = mask)
+    x = x + mha_out
+    x = x + self.ffn(self.norm2(x))
+    return x
